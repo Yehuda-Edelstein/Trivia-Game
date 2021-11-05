bc4e92ea28fb0e19f08b8c859ec6cdddf8641d4e
Yehuda-Edelstein <yeauthor@gmail.com>
2021-11-05 10:54:48 -0400

    Commit 2, more JS tweaking

--- script.js
+++ script.js
@@ -1,18 +1,63 @@
 // VARIABLES
 const startButton = document.querySelector(".start-button")
+let description = document.querySelector(".description")
+
+const answer1 = "1965"
 
 // FUNCTIONS
+
+
+
 function startGame (event) {
     event.preventDefault()
     
     startButton.style.display = "none"
     
-    const description = document.querySelector(".description")
-    var delayInMilliseconds = 2000
+    
+    let delayInMilliseconds = 2000
     description.innerText = "Let the games begin!"
+
     setTimeout(function() {
         description.innerHTML = "Question 1"
-    }, delayInMilliseconds)
+        let triviaQuestion = document.createElement("div")
+        triviaQuestion.classList.add("questions")
+        triviaQuestion.innerText = "In what year was the game of 'trivia' first invented?"
+        document.body.appendChild(triviaQuestion)
+
+        let answerButton1 = document.createElement("button")
+        let answerButton2 = document.createElement("button")
+        let answerButton3 = document.createElement("button")
+
+        answerButton1.innerText = "1965"
+        answerButton2.innerText = "1903"
+        answerButton3.innerText = "1933"
+
+        answerButton1.classList.add("answer-buttons")
+        answerButton2.classList.add("answer-buttons")
+        answerButton3.classList.add("answer-buttons")
+        
+        document.body.appendChild(answerButton1)
+        document.body.appendChild(answerButton2)
+        document.body.appendChild(answerButton3)
+
+        const answerButtons = document.querySelectorAll(".answer-buttons")
+
+        for (let i = 0; i < answerButtons.length; i++) {
+            function chooseAnswer (event) {
+                event.preventDefault()
+                console.log(answerButtons[i].innerText)
+            }
+                answerButtons[i].addEventListener("click", chooseAnswer)
+                
+            
+        }
+
+
+            
+
+
+    }, delayInMilliseconds)    
+    
 }
 
 // EVENT LISTENERS
