d9e9d2f9b138af0ae26ae7d9d7767fb52d33957d
Yehuda-Edelstein <yeauthor@gmail.com>
2021-11-07 19:55:45 -0500

    Home Page and Question 1 Page

--- question-1.js
+++ question-1.js
@@ -2,5 +2,44 @@ const answer1 = document.querySelector("#answer-1")
 const answer2 = document.querySelector("#answer-2")
 const answer3 = document.querySelector("#answer-3")
 
-const answers = [answer1, answer2, answer3]
+nextButton = document.querySelector(".next-question")
+nextButton.style.display = "none"
+
+let isAnswered = false
+
+function firstAnswer (event) {
+    event.preventDefault()
+    if (isAnswered == true) {return}
+    answer1.style.backgroundColor = "green"
+    isAnswered = true
+    nextButton.style.display = "block"
+}
+
+function secondAnswer (event) {
+    event.preventDefault()
+    if (isAnswered == true) {return}
+    answer2.style.backgroundColor = "red"
+    isAnswered = true
+    nextButton.style.display = "block"
+}
+
+function thirdAnswer (event) {
+    event.preventDefault()
+    if (isAnswered == true) {return}
+    answer3.style.backgroundColor = "red"
+    isAnswered = true
+    nextButton.style.display = "block"
+}
+answer1.addEventListener("click", firstAnswer)
+answer2.addEventListener("click", secondAnswer)
+answer3.addEventListener("click", thirdAnswer)
+
+
+
+function nextQuestion (event) {
+    event.preventDefault()
+    
+}
+
+nextButton.addEventListener("click", nextQuestion)
 
